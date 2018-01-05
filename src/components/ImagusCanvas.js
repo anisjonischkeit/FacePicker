661436590cbf57433207ca7bea933a2964633723
Anis Jonischkeit <ajonischkeit@imagus.com.au>
2018-01-05 10:43:14 +1000

    added ability to pass in image

--- src/components/ImagusCanvas.js
+++ src/components/ImagusCanvas.js
@@ -5,10 +5,58 @@ import CanvasState from '../model/CanvasState'
 import type { FaceType } from "../types"
 
 type PropsType = {
-    faces: Array<FaceType>
+    faces: Array<FaceType>,
+    selected: number,
+    imgUrl: ?string,
+    imgElement: ?HTMLImageElement,
+    imgFile: ?HTMLImageElement,
+
 }
  
 type StateType = {}
+
+const imgFromUrl = (url, cb) => {
+    var img = new Image();
+    img.src = url
+    img.onload = function () {
+        cb(img)
+    } 
+}
+
+const imgFromFile = (file, cb) => {
+    imgFromUrl(URL.createObjectURL(file), cb)
+}
+
+const getImageElementFromProps = (props, cb) => {
+    let done = false
+    const errorMsg = "you can only specify one of (imgUrl imgElement imgFile) in ImagusFacePicker"
+    if (props.imgUrl) {
+        done = true
+        imgFromUrl(props.imgUrl, cb)
+    }
+    if (props.imgElement) {
+        if (done) {
+            console.error(errorMsg)
+            throw errorMsg
+        }
+        done = true
+        cb(props.imgElement)
+    } 
+    if (props.imgFile) {
+        if (done) {
+            console.error(errorMsg)
+            throw errorMsg
+        }
+        imgFromFile(props.imgFile, cb)
+        
+    }
+}
+
+function bindFirstArg(fn, a) {
+    return (b) => {
+        return fn(a, b);
+    }
+}
 
 class Canvas extends Component<PropsType, StateType> {
     canvas : ?HTMLCanvasElement = null
@@ -19,7 +67,8 @@ class Canvas extends Component<PropsType, StateType> {
             throw "setFacesInCanvasState should only be called once a CanvasState has been created" 
         }
         this.canvasState.clearFaces()
-        props.faces.forEach((face, idx) => {
+        props.faces &&
+          props.faces.forEach((face, idx) => {
             let selected = false
             if (idx === props.selected) {
                 selected = true
@@ -29,12 +78,14 @@ class Canvas extends Component<PropsType, StateType> {
     }
 
     componentDidMount() {
-        this.canvasState = new CanvasState(this.canvas, this.props.onFacesUpdate)
-        this.setFacesInCanvasState(this.props)
+        getImageElementFromProps(this.props, img => {
+            this.canvasState = new CanvasState(this.canvas, this.props.onFacesUpdate, img)
+            this.setFacesInCanvasState(this.props)
+        })
     }
     
     componentWillReceiveProps(props: PropsType) {
-        this.setFacesInCanvasState(props)
+        getImageElementFromProps(this.props, bindFirstArg(this.setFacesInCanvasState, props))
     }
 
     render() {
